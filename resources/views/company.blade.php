4d740756ef1bbea4c5492b2296efaec6c8d385ec
cuongceopro <cuongceopro@gmail.com>
2017-06-03 20:28:40 +0900

    pushed latest version

--- resources/views/company.blade.php
+++ resources/views/company.blade.php
@@ -5,8 +5,58 @@
 
   <section>
     <h2>会社情報</h2>
+    <br />
+    <h3>会社概要</h3>
+    <table class="ta1">
+    <tr>
+    <th>社名</th>
+    <td>レダクォー合同会社 (Le.Daq LLC)</td>
+    </tr>
+    <tr>
+    <th>所在地</th>
+    <td>〒110-0013 東京都台束区入谷2-3-2-504号</td>
+    </tr>
+    <tr>
+    <th>連絡先</th>
+    <td>Tel: 03-5849-4079<br />Mail: info@ledaq.com</td>
+    </tr>
+    <tr>
+    <th>営業時間</th>
+    <td>10:30~19:00 (最終受付時間)</td>
+    </tr>
+    <tr>
+    <th>事業内容</th>
+    <td>・フルオーダーメード<br />・その他オリジナル商品の生産<br />・販売<br /></td>
+    </tr>
+    <tr>
+    <th>設立</th>
+    <td>2016年6月27日</td>
+    </tr>
+    <tr>
+    <th>代表者</th>
+    <td>レ　ティ　トゥ　フォン </td>
+    </tr>
+    </table>
+    <br />
+
+    <h3>アクセスマップ</h3>
+    <br />
+    <div id="company_map">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3239.3071492645345!2d139.78613651524037!3d35.718663935525704!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188e93e2397b89%3A0xc8da966a7763b7f6!2z44CSMTEwLTAwMTMg5p2x5Lqs6YO95Y-w5p2x5Yy65YWl6LC377yS5LiB55uu77yT4oiS77yS!5e0!3m2!1sja!2sjp!4v1496484303859" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+    </div>
+    <div id="company_access">
+    <p>
+    【住所】：東京都台束区入谷2-3-2-504号 <br />
+    【駅からのアクセス】： <br />
+    　東京メトロ日比谷線入谷駅から5分 <br />
+    　銀座線稲荷駅から15分 <br />
+    　つくばエクスプレス浅草駅から10分 <br />
+    　JR鶯谷駅から13分 <br />
+    </p>
+    </div>
   </section>
 
+  <!--
   <p>
     社名：レダクォー合同会社 (Le.Daq LLC) <br />
     所在地：〒110-0013 東京都台束区入谷2-3-2-504号 <br />
@@ -25,6 +75,7 @@
     　つくばエクスプレス浅草駅から10分 <br />
     　JR鶯谷駅から13分 <br />
   </p>
+-->
 
 </div>
 <!--/main-->
